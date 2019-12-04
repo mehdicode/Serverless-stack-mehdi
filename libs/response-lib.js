bf76546da8b47d0d544b2701d64bdc24b4f9f759
Mahdi Kalan <kalan.mahdi@yahoo.com>
2019-12-04 00:27:15 -0800

    fixed white space issue error ( Trailing spaces not allowed  no-trailing-spaces)

--- libs/response-lib.js
+++ libs/response-lib.js
@@ -1,12 +1,10 @@
 export function success(body) {
     return buildResponse(200, body);
   }
-  
-  export function failure(body) {
+export function failure(body) {
     return buildResponse(500, body);
   }
-  
-  function buildResponse(statusCode, body) {
+function buildResponse(statusCode, body) {
     return {
       statusCode: statusCode,
       headers: {
